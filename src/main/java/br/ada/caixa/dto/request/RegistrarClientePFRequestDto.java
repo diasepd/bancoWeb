3d2895f743b1840b7ce2bda5d1a6cacc36a8e2cc
jcarlosVale <jcarlos.vale@gmail.com>
2024-06-10 20:37:55 +0100

    security desabilitado teste integrado

--- src/main/java/br/ada/caixa/dto/request/RegistrarClientePFRequestDto.java
+++ src/main/java/br/ada/caixa/dto/request/RegistrarClientePFRequestDto.java
@@ -1,12 +1,18 @@
 package br.ada.caixa.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 
-@Getter
-@Setter
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegistrarClientePFRequestDto {
 
     private String cpf;
